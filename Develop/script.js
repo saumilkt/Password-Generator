efc86de5ada9b3515b7d78d94a5fb200cecf80e8
saumilkt <saumilkt@gmail.com>
2021-07-09 17:16:36 -0700

    adding lowercase uppercase and numeric chars to js file

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,8 @@
 // Assignment code here
+var upperArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numericArray = ["1","2","3","4","5","6","7","8","9","0"];
+var specialArray = [];
 
 
 // Get references to the #generate element
